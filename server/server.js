38a9f27a38511b1137811312e2f421e38ecf82d9
Hilal ahmad <86137247+hilalahmad32@users.noreply.github.com>
2022-03-01 23:07:38 -0800

    port change

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 const cors = require('cors');
 require('./connection/config.js');
 app.use(cors());
